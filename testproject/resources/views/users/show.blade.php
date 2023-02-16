64b919919e4e7463e3346fc0451e1cceace43f43
rintarohida <rintarohida1@gmail.com>
2023-02-17 03:17:07 +0900

    メール認証の追加(作りかけ)

--- testproject/resources/views/users/show.blade.php
+++ testproject/resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 {{ $user->corp->corp_name??"無所属"}}</p>
 <a href="./{{$user->id}}/edit">名前編集</a></p>
 <a href="./{{$user->id}}/applyform">会社変更</a>
-<form action="{{ route('post.destroy', $user->id) }}" method="POST">
+<form action="{{ route('users.destroy', $user->id) }}" method="POST">
          @csrf
          @method("DELETE")
          <button type="submit" class="btn btn-danger">削除</button>
