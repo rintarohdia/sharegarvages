e28d6b48c407eb09e1f4fb9167503f35b24de85b
rintarohida <rintarohida1@gmail.com>
2023-02-18 00:20:07 +0900

    Revert "メール認証の追加(作りかけ)"
    This reverts commit 64b919919e4e7463e3346fc0451e1cceace43f43.

--- testproject/app/Models/User.php
+++ testproject/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
- use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\corp;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
